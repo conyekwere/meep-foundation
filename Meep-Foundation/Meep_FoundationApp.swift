0a821068aa113a8d964aa47dd2e725cb0811c206
Your Name <you@example.com>
2025-01-21 23:37:43 -0500

    Initial Commit

---
+++ Meep-Foundation/Meep_FoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meep_FoundationApp.swift
+//  Meep-Foundation
+//
+//  Created by Chima onyekwere on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Meep_FoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meep-FoundationTests/Meep_FoundationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meep_FoundationTests.swift
+//  Meep-FoundationTests
+//
+//  Created by Chima onyekwere on 1/21/25.
+//
+
+import Testing
+@testable import Meep_Foundation
+
+struct Meep_FoundationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
