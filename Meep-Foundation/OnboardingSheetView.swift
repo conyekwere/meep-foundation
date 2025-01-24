de6110fa8f75d7ca182d864f9247f157e35964df
Your Name <you@example.com>
2025-01-24 02:54:38 -0500

    added search button and 1/3 or search meeting input ui

--- Meep-Foundation/OnboardingSheetView.swift
+++ Meep-Foundation/OnboardingSheetView.swift
@@ -11,188 +11,96 @@ struct OnboardingSheetView: View {
     @ObservedObject var viewModel: MeepViewModel
     @Environment(\.colorScheme) var colorScheme
     @Binding var isLocationAllowed: Bool
-
+    @Binding var searchRequest: Bool
 
     var body: some View {
-        ZStack{
-            VisualEffectBlur(blurStyle: colorScheme == .dark ? .systemUltraThinMaterialDark : .systemUltraThinMaterialLight)
-                .cornerRadius(16)
-                .ignoresSafeArea(edges: .bottom)
-            
+        ZStack {
             VStack(spacing: 16) {
                 // Drag Handle
                 Capsule()
                     .frame(width: 40, height: 5)
                     .foregroundColor(Color(.lightGray).opacity(0.4))
                 
-                
                 ScrollView {
-                    VStack(spacing:24) {
-                        VStack{
-                            ZStack {
-                                LinearGradient(
-                                    gradient: Gradient(colors: [Color(hex: "67DB92"),Color(hex: "41AC99")]),
-                                    startPoint: .top,
-                                    endPoint: .bottom
-                                )
-                                
-                                VStack(spacing:8) {
+                    VStack(spacing: 24) {
+                        // First Card: Allow Location
+                        OnboardingCardView(
+                            title: "Allow location",
+                            subtitle: "Allow location to get from A to B",
+                            gradientColors: [Color(hex: "67DB92"), Color(hex: "41AC99")],
+                            icon: AnyView(
+                                ZStack {
+                                    Circle()
+                                        .fill(Color(#colorLiteral(red: 0.3455161154270172, green: 0.5001650452613831, blue: 0.9254494905471802, alpha: 1)))
+                                        .strokeBorder(Color.white, lineWidth: 1)
+                                        .frame(width: 16, height: 16)
+                                        .shadow(color: Color.black.opacity(0.3), radius: 2, x: 0, y: 1)
                                     
-                                    ZStack {
-                                        Circle()
-                                            .fill(Color(#colorLiteral(red: 0.3455161154270172, green: 0.5001650452613831, blue: 0.9254494905471802, alpha: 1)))
-                                            .strokeBorder(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
-                                            .frame(width: 16, height: 16)
-                                            .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.30000001192092896)), radius:2.1352040767669678, x:0, y:0.7117347121238708)
-                                        
-                                        Circle()
-                                            .strokeBorder(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
-                                            .frame(width: 60, height: 60)
-                                        
-                                        Circle()
-                                            .strokeBorder(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
-                                            .frame(width: 108, height: 108)
-                                        
-                                    }
-                                    .padding(.bottom, 16)
+                                    Circle()
+                                        .strokeBorder(Color.white, lineWidth: 1)
+                                        .frame(width: 60, height: 60)
                                     
-                                    Text("Allow location")
-                                        .font(.title)
-                                        .fontWeight(.bold)
-                                        .fontWidth(.expanded)
-                                        .foregroundColor(.white)
-                                        .lineLimit(2)
-                                        .multilineTextAlignment(.center)
-                                        .textCase(.uppercase)
-                                        .lineSpacing(14)
-                                        .minimumScaleFactor(0.9)
-                                    
-                                    
-                                    Text("Allow location to get from A to B")
-                                        .font(.title3)
-                                        .foregroundColor(Color(.white))
-                                        .padding(.bottom, 24)
-                                        .lineLimit(1)
-                                        .minimumScaleFactor(0.9)
-                                    Button(action: {
-                                        
-                                        viewModel.requestUserLocation()
-                                        withAnimation {
-                                        }
-                                    }) {
-                                        Text("Allow")
-                                            .font(.title3)
-                                            .foregroundColor(Color(.darkGray))
-                                            .padding()
-                                            .frame(maxWidth: .infinity)
-                                            .background(Color(.white))
-                                            .cornerRadius(10)
-                                    }
-                                    .frame(height:44)
+                                    Circle()
+                                        .strokeBorder(Color.white, lineWidth: 1)
+                                        .frame(width: 108, height: 108)
                                 }
-                                .padding(24)
-                                
-                                
+                            ),
+                            actionTitle: "Allow",
+                            action: {
+                                viewModel.requestUserLocation()
                             }
-                            
-                        }
+                        )
+                        .cornerRadius(16)
+                        .padding(.horizontal, 16)
                         .frame(height: 360)
-                        .cornerRadius(12)
-                        .padding(.horizontal,16)
-                        
-                        VStack() {
-                            ZStack {
-                                LinearGradient(
-                                    gradient: Gradient(colors: [Color(hex: "145492"),Color(hex: "3D9FF5")]),
-                                    startPoint: .top,
-                                    endPoint: .bottom
-                                )
-                                
-                                VStack(spacing:16) {
+                    
+
+                        // Second Card: Meet Your Friends
+                        OnboardingCardView(
+                            title: "Meet Your Friends Halfway",
+                            subtitle: "Search the Perfect Meeting Point",
+                            gradientColors: [Color(hex: "145492"), Color(hex: "3D9FF5")],
+                            icon: AnyView(
+                                ZStack {
+                                    Image(systemName: "mappin.circle")
+                                        .resizable()
+                                        .frame(width: 34, height: 34)
+                                        .foregroundStyle(Color.white)
                                     
-                                    ZStack {
-                                        Image(systemName: "mappin.circle")
-                                            .resizable()
-                                            .frame(width: 34, height: 34)
-                                            .foregroundStyle(Color.white)
-                                        
-                                        Circle()
-                                            .strokeBorder(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)).opacity(0.5),  style: StrokeStyle(lineWidth: 3, dash: [4,4]))
-                                            .frame(width: 74, height: 74)
-                                        
-                                        Circle()
-                                            .strokeBorder(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)).opacity(0.5), lineWidth: 2)
-                                            .frame(width: 108, height: 108)
-                                        
-                                    }
-                                    .padding(.bottom, 8)
+                                    Circle()
+                                        .strokeBorder(Color.white.opacity(0.5), style: StrokeStyle(lineWidth: 3, dash: [4, 4]))
+                                        .frame(width: 74, height: 74)
                                     
-                                    
-                                    Text("Meet Your Friends Halfway")
-                                        .font(.title)
-                                        .fontWeight(.bold)
-                                        .fontWidth(.expanded)
-                                        .foregroundColor(.white)
-                                        .lineLimit(2)
-                                        .multilineTextAlignment(.center)
-                                        .textCase(.uppercase)
-                                        .lineSpacing(14)
-                                        .minimumScaleFactor(0.9)
-                                    
-                                    
-                                    Text("Search the Perfect Meeting Point")
-                                        .font(.title3)
-                                        .foregroundColor(Color(.white))
-                                        .padding(.bottom, 10)
-                                        .lineLimit(1)
-                                        .minimumScaleFactor(0.9)
-                                    Button(action: {
-                                        // Simulate search action
-                                        withAnimation {
-                                            
-                                        }
-                                    }) {
-                                        Text("Search")
-                                            .font(.title3)
-                                            .foregroundColor(Color(.darkGray))
-                                            .padding()
-                                            .frame(maxWidth: .infinity)
-                                            .background(Color(.white))
-                                            .cornerRadius(10)
-                                    }
-                                    .frame(height:44)
-                                    .padding(.top, 8)
-                                    .padding(.bottom, 16)
+                                    Circle()
+                                        .strokeBorder(Color.white.opacity(0.5), lineWidth: 2)
+                                        .frame(width: 108, height: 108)
                                 }
-                                .padding(24)
-                                
-                                
-                                
+                            ),
+                            actionTitle: "Search",
+                            action: {
+                                // Add search action here
                             }
-                            
-                        }
+                        )
+                        .cornerRadius(16)
+                        .padding(.horizontal, 16)
                         .frame(height: 390)
-                        .cornerRadius(12)
-                        .padding(.horizontal,16)
+                        .padding(.bottom, 64)
+                  
+                        Spacer(minLength: 16)
                     }
+                    .ignoresSafeArea(edges: .bottom)
                 }
-                
             }
             .padding(.top, 8)
-            .padding(.bottom, 32)
-
-            .cornerRadius(16)
-            .ignoresSafeArea(edges: .bottom)
-            
+           
 
         }
-        
     }
 }
 
-
 #Preview {
-    OnboardingSheetView(viewModel: MeepViewModel(), isLocationAllowed:  .constant(false)) // Replace MeepViewModel() with your mock data
-                .previewLayout(.sizeThatFits) // Ensures the bottom sheet view fits the preview size
-                .background(Color.gray.opacity(0.2)) // Optional background to visualize contrast
+    OnboardingSheetView(viewModel: MeepViewModel(), isLocationAllowed: .constant(false), searchRequest: .constant(false))
+        .previewLayout(.sizeThatFits)
+        .background(Color.gray.opacity(0.2))
+        .ignoresSafeArea(edges: .bottom)
 }
