579d1792e2862d25fe6fbddfde8c495e2d32929a
Chima Onyekwere <madebychima@mac.mynetworksettings.com>
2025-05-22 01:58:17 -0400

    Apply .gitignore and clean tracked files

--- Pods/Target Support Files/Pods-Meep-Foundation/Pods-Meep-Foundation-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Meep_Foundation : NSObject
-@end
-@implementation PodsDummy_Pods_Meep_Foundation
-@end
--- Pods/Target Support Files/Pods-Meep-FoundationTests/Pods-Meep-FoundationTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Meep_FoundationTests : NSObject
-@end
-@implementation PodsDummy_Pods_Meep_FoundationTests
-@end
